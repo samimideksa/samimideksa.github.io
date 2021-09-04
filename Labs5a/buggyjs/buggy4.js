a3bc7015b5fa295d857f2e1345fac1081c0a83cb
Samuel Mideksa <samicoursera@gmail.com>
2021-09-03 22:02:02 -0500

    added Labs5a, tip calculator and On Click alert exercise

---
+++ Labs5a/buggyjs/buggy4.js
@@ -0,0 +1,12 @@
+function gogogo() {
+    alert("Yay, it works!");
+}
+
+function somethingElse() {
+    if (5 > 2) {
+        alert("Bigger");
+    }
+    if (5 < 2) {
+        alert("Smaller");
+    }
+}
